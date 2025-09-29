ade9ed6d433cebc7a3420969ee6801fd64b6b08c
Gabriel Santos Silva <gabriel.s.silva95@aluno.senai.br>
2025-09-29 15:38:51 -0300

    criei a função de atualizar o banco e coloquei algumas pessoas no banco

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ cursor.execute("""
     disponivel TEXT CHECK(disponivel IN ('sim', 'não'))
     )
 """)
+biblioteca = [
+   ("Enzo", "a", 2, "sim"),
+    ("Murilo", "b", 3, "não"),
+    ("Eduardo", "b", 4, "sim")
+]
+
+cursor.executemany("""
+    INSERT INTO biblioteca (titulo, autor, ano, disponivel)
+    VALUES (?, ?, ?, ?)
+""", biblioteca)
+conexao.commit()
 def adicionando():
     titulo = input("Digite o nome do livro que deseja cadastrar: ")
     autor = input("Digite o nome do autor:  ")
@@ -20,5 +31,22 @@ def adicionando():
         VALUES (?, ?, ?, ?)
     """, (titulo, autor, ano, disponivel))
     conexao.commit()
+
 def mostra_lista(linha):
     print(f"ID: {linha[0]} | TITULO: {linha[1]} | AUTOR: {linha[2]} | ANO: {linha[3]} | DISPONIVEL: {linha[4]}")
+
+def atualizar_banco():
+    print(mostra_lista)
+
+    id_livro = input("Digite o ID do livro que deseja atualizar: ")
+
+    disponivel = input("O livro esta disponivel so resposta de 'sim' or 'não':")
+
+    cursor.execute("""
+    UPDATE biblioteca
+    SET disponivel = ?
+    WHERE id = ?    
+    """,(disponivel, id_livro))
+
+    conexao.commit()
+    print("Status do livro atualizado") 
