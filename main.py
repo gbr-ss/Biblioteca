a8469a9ea6671cd1ed02b377ccfe11add5eb0a4e
Gabriel Santos Silva <gabriel.s.silva95@aluno.senai.br>
2025-09-29 14:35:43 -0300

    função de adicionar criado

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import sqlite3
 conexao = sqlite3.connect("biblioteca.db")
 cursor = conexao.cursor()
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS alunos(
+    CREATE TABLE IF NOT EXISTS biblioteca(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    título TEXT NOT NULL,
-    autor TEXT NOT NULL       
+    titulo TEXT NOT NULL,
+    autor TEXT NOT NULL,       
     ano INTEGER,
     disponivel TEXT CHECK(disponivel IN ('sim', 'não'))
-        
-                """)
+    )
+""")
+def adicionando():
+    titulo = input("Digite o nome do livro que deseja cadastrar: ")
+    autor = input("Digite o nome do autor:  ")
+    ano = int(input("Digite o ano de lançamento do livro:  "))
+    disponivel = "sim"  # Define o valor padrão para 'disponivel'
+    cursor.execute("""
+        INSERT INTO biblioteca (titulo, autor, ano, disponivel)
+        VALUES (?, ?, ?, ?)
+    """, (titulo, autor, ano, disponivel))
+    conexao.commit()
